563dec631e06a24c7123c6be8e19a66a184ae8ed
Paul Szadurski <paulszadurski2@gmail.com>
2021-04-30 21:20:07 -0400

    please work

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const dealsUrl = "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=
 const storesUrl = "https://www.cheapshark.com/api/1.0/stores"
 
 fetch(dealsUrl).then(function (response) {
-    return response.json();
+  return response.json();
 }).then(function (returned) {
-    console.log(returned);
-    console.log(returned[8].salePrice);
+  console.log(returned.length);
+  console.log(returned[8].salePrice);
+  for (i = 0; i < 10; i++) {
+    console.log(returned[i].salePrice);
+  }
+  // console.log( M)
 })
 
 fetch(storesUrl).then(function (response) {
-    return response.json();
+  return response.json();
 }).then(function (result) {
-    console.log(result)
-    console.log(result[3].storeName);
+  console.log(result)
+  console.log(result[3].storeName);
 })
